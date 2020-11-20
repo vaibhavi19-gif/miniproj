8800efc09b87322e3d6b165f10d889c6340c5d05
Shreya Kulkarni <61427019+srk129@users.noreply.github.com>
2020-11-20 22:00:10 +0530

    Add files via upload

---
+++ student_shreya_new/include/sidebar.php
@@ -0,0 +1,39 @@
+
+<div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <span data-feather="home"></span>
+                  Dashboard <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              
+              
+              <li class="nav-item">
+                <a class="nav-link" href="student.php">
+                  <span data-feather="users"></span>
+                  Student Details
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Points Completed
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="bar-chart-2"></span>
+                  Intenships/Jobs
+                </a>
+              </li>
+              
+            </ul>
+
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              
+          </div>
+        </nav>
