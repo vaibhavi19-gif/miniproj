f3882ef160d78e820082202e5b3f2a9697eb6022
Shreya Kulkarni <61427019+srk129@users.noreply.github.com>
2020-11-20 21:59:06 +0530

    Add files via upload

---
+++ teacher_shreya_new/include/sidebar.php
@@ -0,0 +1,37 @@
+
+<div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <span data-feather="home"></span>
+                  Dashboard <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="jobgiver.php">
+                  <span data-feather="users"></span>
+                  Intership Giver's Details
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Students Applied
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Students Confirmed
+                </a>
+              </li>
+              
+            </ul>
+
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              
+          </div>
+        </nav>
