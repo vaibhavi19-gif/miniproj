4eca8739b4fd0b05ad22e70901a0241b444d3c31
Shreya Kulkarni <61427019+srk129@users.noreply.github.com>
2020-11-22 22:53:34 +0530

    Add files via upload

---
+++ student_final/include/sidebar.php
@@ -0,0 +1,47 @@
+<div class="container-fluid mt-0">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block sidebar" style="margin-top:0rem;padding-top:0rem">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="studentdashboard.php" style="color:purple;font-size:2rem">
+                  <span data-feather="home"></span>
+                  Dashboard <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              
+              
+              <li class="nav-item">
+                <a class="nav-link" href="student.php" style="color:purple;font-size:2rem">
+                  <span data-feather="users"></span>
+                  Student Details
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style="color:purple;font-size:2rem">
+                  <span data-feather="users"></span>
+                  Points Completed
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="internship_applied.php" style="color:purple;font-size:2rem">
+                  <span data-feather="bar-chart-2"></span>
+                  Intenships/Jobs Applied
+                </a>
+              </li>
+              
+            </ul>
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              
+          </div>
+        </nav>
+
+        <!--<script>
+        $(function() {
+          $('a.nav-link').click(function() {
+            $('.nav-link').removeClass('active');
+            //$('.a-class-name').removeClass('a-class-name');
+            $(this).addClass('active');
+          });
+        });
+        </script>-->
