78001f5af954539e65d4281a7e2d042953037bce
Vaibhavi Kundle <60182895+vaibhavi19-gif@users.noreply.github.com>
2020-11-22 11:20:44 +0530

    Add files via upload

---
+++ vai_changes/student/sidebar.php
@@ -0,0 +1,39 @@
+
+<div class="container-fluid mt-0">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block sidebar" style="margin-top:0rem;padding-top:0rem">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="studentdashboard.php" style="color:purple;font-size:2rem">
+                  <span data-feather="home"></span>
+                  Dashboard <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              
+              
+              <li class="nav-item">
+                <a class="nav-link" href="student.php" style="color:purple;font-size:2rem">
+                  <span data-feather="users"></span>
+                  Student Details
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style="color:purple;font-size:2rem">
+                  <span data-feather="users"></span>
+                  Points Completed
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style="color:purple;font-size:2rem">
+                  <span data-feather="bar-chart-2"></span>
+                  Intenships/Jobs
+                </a>
+              </li>
+              
+            </ul>
+
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              
+          </div>
+        </nav>
